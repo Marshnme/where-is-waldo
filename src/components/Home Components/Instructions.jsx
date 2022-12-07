8d9cc4cd4b9d37f7d99c49b32e6f3a8bd291f765
Joshua Holtsclaw <jildsmiles@yahoo.com>
2022-12-07 13:29:41 -0500

    added instructions to home screen

--- src/components/Home Components/Instructions.jsx
+++ src/components/Home Components/Instructions.jsx
@@ -1,7 +1,15 @@
 
 const Instructions = () => {
     return (
-        <h1>Instructions</h1>
+        <>
+            <h3>Instructions</h3>
+            <ul>
+                <li>On the right of your screen will be three items you need to find in the image to your left.</li>
+                <li>Find one of the three items.</li>
+                <li>Click on the item to open the choice menu and choose the appropriate item.</li>
+                <li>Repeat until you have found all the items.</li>
+            </ul>
+        </>
     )
 }
 
