dee977e03c9ce8d03e03baabbfc5382b03d8ee11
Joshua Holtsclaw <jildsmiles@yahoo.com>
2022-12-10 16:55:11 -0500

    rendering img and objectives on game screen

---
+++ src/components/Game Components/Objectives.jsx
@@ -0,0 +1,16 @@
+
+const Objectives = () => {
+    return (
+        <>
+            <h3>Objectives</h3>
+            <p>You need to find:</p>
+            <ul>
+                <li>Koopa</li>
+                <li>DK Barrel</li>
+                <li>Red Shy Guy</li>
+            </ul>
+        </>
+    )
+}
+
+export default Objectives
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,18 @@
-
+import ImageOverview from "./Home Components/ImageOverview"
+import Objectives from "./Game Components/Objectives"
+import '../stylesheets/css/game.css'
 const Game = () => {
     return (
-        <h1>game</h1>
+        <>
+            <div className="game-container">
+                <div className="img-container">
+                    <ImageOverview></ImageOverview>
+                </div>
+                <div className="game-obj">
+                    <Objectives></Objectives>
+                </div>
+            </div>
+        </>
     )
 }
 
