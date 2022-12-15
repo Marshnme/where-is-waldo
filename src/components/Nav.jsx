197d79a2f149455206e887e1fc8fd721c660046a
Marshnme <jildsmiles@yahoo.com>
2022-12-14 23:27:56 -0500

    added basic color scheme, working on img size for game page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import '../stylesheets/css/nav.css'
 const Nav = () => {
     return (
         <div>
-            <p>Wheres Wario</p>      
+            <p>Wheres Wario?</p>      
         </div>
     )
 }
